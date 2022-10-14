42aad3ae9ba5a77f175eda1f074c594a76276218
Sivasree5 <113752242+Sivasree5@users.noreply.github.com>
2022-10-14 11:23:52 +0530

    t=int(input()) for i in range (t):     n=int(input())     a=n     while True:         is_prime=True         for j in range(2,int(a**0.5)+1):             if a%j==0:                 is_prime=False                 break         if is_prime==True:             break         else:             a+=1     b=n     while True:         is_prime=True         for j in range(2,int(b**0.5)+1):             if b%j==0:                 is_prime=False                 break         if is_prime==True:             break         else:             b-=1     d=a-n     c=n-b     if a-n < n-b:         print(a)     else:         print(b)

---
+++ Nearest_Prime.py
@@ -0,0 +1,31 @@
+t=int(input())
+for i in range (t):
+    n=int(input())
+    a=n
+    while True:
+        is_prime=True
+        for j in range(2,int(a**0.5)+1):
+            if a%j==0:
+                is_prime=False
+                break
+        if is_prime==True:
+            break
+        else:
+            a+=1
+    b=n
+    while True:
+        is_prime=True
+        for j in range(2,int(b**0.5)+1):
+            if b%j==0:
+                is_prime=False
+                break
+        if is_prime==True:
+            break
+        else:
+            b-=1
+    d=a-n
+    c=n-b
+    if a-n < n-b:
+        print(a)
+    else:
+        print(b)
